ad687de202d53b674b9e737a042c6c97de0b3d44
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-04-17 15:48:27 +0200

    examples

--- server/src/iec62209_service/api_routes.py
+++ server/src/iec62209_service/api_routes.py
@@ -1,14 +1,42 @@
 from fastapi import APIRouter
+from pydantic import BaseModel, Field, conint
+from enum import Enum
+from fastapi import File, UploadFile
+from iec62209 import Work
 
-# from iec62209 import Work
+#
+# API Models
+#
 
+
+class Health(BaseModel):
+    x: bool
+    y: int
+    z: conint(ge=2)
+
+
+
+class MyEnum(str, Enum):
+    FOO = "FOO"
+    BAR = "BAR"
+
+
+#
+# API Handlers
+#
 
 router = APIRouter()
 
 
-@router.post("/health")
-async def health():
-    ...
+@router.post("/health/{name}", response_model=Health)
+async def health(health: Health, name: str, enabled: MyEnum = MyEnum.BAR):
+
+    return Health(x=True, y=3, z=33)
+
+
+@router.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
 
 
 @router.post("/sample")
