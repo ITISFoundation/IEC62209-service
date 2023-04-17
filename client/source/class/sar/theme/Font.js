f93db3b835f2634cba9f0d265ee0c11543cbd30d
odeimaiz <maiz@itis.swiss>
2023-04-17 17:34:14 +0200

    npm init

---
+++ client/source/class/sar/theme/Font.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright: 2023 undefined
+
+   License: MIT license
+
+   Authors: undefined
+
+************************************************************************ */
+
+qx.Theme.define("sar.theme.Font",
+{
+  extend : qx.theme.indigo.Font,
+
+  fonts :
+  {
+  }
+});
