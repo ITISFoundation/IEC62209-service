4eb8f9237a6b29e9322013505380ba4b07868c20
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-04-20 19:10:15 +0200

    adds i/o settings

--- server/src/iec62209_service/settings.py
+++ server/src/iec62209_service/settings.py
@@ -1,9 +1,46 @@
+import os
 from pathlib import Path
 
 from pydantic import BaseSettings, validator
 
 
-class ApplicationSettings(BaseSettings):
+class OsparcServiceSettings(BaseSettings):
+
+    INPUT_FOLDER: Path | None = None
+    OUTPUT_FOLDER: Path | None = None
+    LOG_FOLDER: Path | None = None
+
+    @validator("INPUT_FOLDER", "OUTPUT_FOLDER")
+    @classmethod
+    def check_dir_exists(cls, v):
+        if v is not None and not v.exists():
+            raise ValueError(
+                f"Folder {v} does not exists."
+                "Expected predefined and created by sidecar"
+            )
+        return v
+
+    @validator("INPUT_FOLDER")
+    @classmethod
+    def check_input_dir(cls, v):
+        if v is not None:
+            f = v / "inputs.json"
+            if not f.exists():
+                raise ValueError(
+                    f"File {f} does not exists."
+                    "Expected predefined and created by sidecar"
+                )
+        return v
+
+    @validator("OUTPUT_FOLDER")
+    @classmethod
+    def check_output_dir(cls, v: Path):
+        if v is not None and not os.access(v, os.W_OK):
+            raise ValueError(f"Do not have write access to {v}: {v.stat()}")
+        return v
+
+
+class ApplicationSettings(OsparcServiceSettings):
     CLIENT_OUTPUT_DIR: Path
 
     @validator("CLIENT_OUTPUT_DIR")
