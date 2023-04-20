4ede98df48fd7fbfc1efb471f858ab0e7a3af19b
odeimaiz <maiz@itis.swiss>
2023-04-20 10:17:04 +0200

    sar.io.Resources

---
+++ client/source/class/sar/io/Resources.js
@@ -0,0 +1,156 @@
+/* ************************************************************************
+
+   Copyright:
+     2023 IT'IS Foundation, https://itis.swiss
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+
+   Authors:
+     * Odei Maiz (odeimaiz)
+
+************************************************************************ */
+
+/**
+ * Class that defines all the endpoints of the API to get the application resources. It also offers some convenient methods
+ * to get them. It stores all the data in {osparc.store.Store} and consumes it from there whenever it is possible. The flag
+ * "useCache" must be set in the resource definition.
+ *
+ * *Example*
+ *
+ * Here is a little example of how to use the class. For making calls that will update or add resources in the server,
+ * such as POST and PUT calls. You can use the "fetch" method. Let's say you want to modify a study using POST.
+ *
+ * <pre class='javascript'>
+ *   const params = {
+ *     url: { // Params for the URL
+ *       studyId
+ *     },
+ *     data: { // Payload
+ *       studyData
+ *     }
+ *   }
+ *   osparc.data.Resources.post("trainingTestGeneration", "getOne", params)
+ *     .then(study => {
+ *       // study contains the new updated study
+ *       // This code will execute if the call succeeds
+ *     })
+ *     .catch(err => {
+ *       // Treat the error. This will execute if the call fails.
+ *     });
+ * </pre>
+ */
+
+
+qx.Class.define("sar.io.Resources", {
+  extend: qx.core.Object,
+  type: "singleton",
+
+  defer: function(statics) {
+    statics.resources = {
+      /*
+       * TRAINING SET GENERATION
+       */
+      "trainingTestGeneration": {
+        endpoints: {
+          create: {
+            method: "POST",
+            url: "/training-test-generation:create"
+          },
+          xport: {
+            method: "GET",
+            url: "/training-test-generation:xport"
+          },
+        }
+      },
+      /*
+       * Analysis & Creation
+       */
+      "analysisCreation": {
+        load: {
+          create: {
+            method: "POST",
+            url: "/analysis-creation/model:load"
+          },
+          create: {
+            method: "POST",
+            url: "/analysis-creation:create"
+          },
+          xport: {
+            method: "GET",
+            url: "/analysis-creation:xport"
+          },
+        }
+      }
+    }
+  },
+
+  statics: {
+    fetch: function(resource, endpoint, params, options = {}) {
+      return this.getInstance().fetch(resource, endpoint, params, options);
+    }
+  },
+
+  members: {
+    /**
+     * Method to fetch resources from the server. If configured properly, the resources in the response will be cached in {osparc.store.Store}.
+     * @param {String} resource Name of the resource as defined in the static property 'resources'.
+     * @param {String} endpoint Name of the endpoint. Several endpoints can be defined for each resource.
+     * @param {Object} params Object containing the parameters for the url and for the body of the request, under the properties 'url' and 'data', respectively.
+     * @param {Object} options Collections of options
+     */
+    fetch: function(resource, endpoint, params = {}, options = {}) {
+      return new Promise((resolve, reject) => {
+        if (this.self().resources[resource] == null) {
+          reject(Error(`Error while fetching ${resource}: the resource is not defined`));
+        }
+
+        const resourceDefinition = this.self().resources[resource];
+        const res = new qx.io.rest.Resource(resourceDefinition.endpoints);
+        res.configureRequest(request => {
+          const headers = [{
+            key: "Accept",
+            value: "application/json"
+          }];
+          headers.forEach(item => request.setRequestHeader(item.key, item.value));
+          request.setRequestHeader("Content-Type", "application/json");
+        });
+
+        if (!res.includesRoute(endpoint)) {
+          reject(Error(`Error while fetching ${resource}: the endpoint is not defined`));
+        }
+
+        res.addListenerOnce(endpoint + "Success", e => {
+          const response = e.getRequest().getResponse();
+          const data = response.data;
+          res.dispose();
+          "resolveWResponse" in options && options.resolveWResponse ? resolve(response) : resolve(data);
+        }, this);
+
+        res.addListenerOnce(endpoint + "Error", e => {
+          let message = null;
+          let status = null;
+          if (e.getData().error) {
+            const logs = e.getData().error.logs || null;
+            if (logs && logs.length) {
+              message = logs[0].message;
+            }
+            status = e.getData().error.status;
+          } else {
+            const req = e.getRequest();
+            message = req.getResponse();
+            status = req.getStatus();
+          }
+          res.dispose();
+          const err = Error(message ? message : `Error while trying to fetch ${endpoint} ${resource}`);
+          if (status) {
+            err.status = status;
+          }
+          reject(err);
+        });
+
+        res[endpoint](params.url || null, params.data || null);
+      });
+    }
+  }
+});
