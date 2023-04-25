80a8cafd08b2e28885fe6d5abaaae4b6028be6a6
odeimaiz <maiz@itis.swiss>
2023-04-25 11:02:19 +0200

    downloadJson

--- client/source/class/sar/steps/Utils.js
+++ client/source/class/sar/steps/Utils.js
@@ -111,12 +111,9 @@ qx.Class.define("sar.steps.Utils", {
     },
 
     downloadJson: function (data, fileName) {
-      const blob = new Blob([data], {
-        type: "text/json"
-      });
-      const url = window.URL.createObjectURL(blob);
+      const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
       const a = document.createElement("a");
-      a.setAttribute("href", url);
+      a.setAttribute("href", dataStr);
       a.setAttribute("download", fileName);
       a.click();
     },
