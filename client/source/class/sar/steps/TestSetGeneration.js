c9465160a7e193365f3362f214b5336df47b4fb2
odeimaiz <maiz@itis.swiss>
2023-04-28 09:40:06 +0200

    updated text for "Test Set Generation"

--- client/source/class/sar/steps/TestSetGeneration.js
+++ client/source/class/sar/steps/TestSetGeneration.js
@@ -23,9 +23,8 @@ qx.Class.define("sar.steps.TestSetGeneration", {
     // overriden
     _getDescriptionText: function() {
       return "\
-        Generates a random latin hypercube sample with 8 dimensions and saves the results to a .csv file. The 8 test variables are:\
-        <br>Frequency, output power, peak to average power ratio (PAPR), bandwidth (BW), distance (mm), angle (deg), x (mm), and y (mm).\
-        <br><br>When performing the SAR measurements, fill in the SAR (SAR1g and/or SAR10g), and uncertainty (U1g and/or U10g) values. The uncertainty values should be reported with a 95% confidence level (k = 2 standard deviations).\
+        Enter the measurement area and number of samples, then select 'Create Test data'. This will generate the set of test conditions to create the GPI model. These are shown as a list (in the 'Data' tab) and plotted to show the distributions for different dimensions (in the 'Distribution' tab).\
+        <br><br>Click 'Export test data' to export the set to a CSV file. Measure each of these test conditions and fill in the sar10g and u10g columns. The uncertainty values should be reported with a 95% confidence level (k = 2 standard deviations).\
       "
     },
 
