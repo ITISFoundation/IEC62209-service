34e96035b3228e6d7bf6dfc5e7cfcc710461a29d
odeimaiz <maiz@itis.swiss>
2023-05-05 14:57:29 +0200

    dummy -> timestamp

--- server/src/iec62209_service/routers/confirm_model.py
+++ server/src/iec62209_service/routers/confirm_model.py
@@ -51,9 +51,9 @@ async def confirm_model_qqplot(timestamp: str = "") -> Response:
 
 
 @router.get("/deviations", response_class=Response)
-async def confirm_model_deviations(dummy: str = "") -> Response:
-    if not dummy:
-        # dummy parameter to avoid browser caching the plot
+async def confirm_model_deviations(timestamp: str = "") -> Response:
+    if not timestamp:
+        # timestamp parameter to avoid browser caching the plot
         return Response(status_code=status.HTTP_400_BAD_REQUEST)
     try:
         buf = SampleInterface.testSet.plot_deviations()
